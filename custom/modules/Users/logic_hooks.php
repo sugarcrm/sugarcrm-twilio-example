5019551b1c277bb52db3e7d6d158cb72b25af384
John Mertic <jmertic@gmail.com>
2013-08-15 13:38:49 -0400

    Updated license header

--- custom/modules/Users/logic_hooks.php
+++ custom/modules/Users/logic_hooks.php
@@ -1,6 +1,35 @@
 <?php
-// Do not store anything in this file that is not part of the array or the hook version.  This file will	
-// be automatically rebuilt in the future. 
+
+/**
+
+Copyright 2013 SugarCRM. All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification, are
+permitted provided that the following conditions are met:
+
+   1. Redistributions of source code must retain the above copyright notice, this list of
+      conditions and the following disclaimer.
+
+   2. Redistributions in binary form must reproduce the above copyright notice, this list
+      of conditions and the following disclaimer in the documentation and/or other materials
+      provided with the distribution.
+
+THIS SOFTWARE IS PROVIDED BY SUGARCRM ``AS IS'' AND ANY EXPRESS OR IMPLIED
+WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL SUGARCRM OR
+CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+The views and conclusions contained in the software and documentation are those of the
+authors and should not be interpreted as representing official policies, either expressed
+or implied, of SugarCRM.
+
+*/
+
  $hook_version = 1; 
 $hook_array = Array(); 
 // position, file, function 
@@ -10,6 +39,3 @@ $hook_array['after_login'][] = Array(1, 'Load user into Twilio iFrame to enable 
 
 $hook_array['before_logout'] = Array(); 
 $hook_array['before_logout'][] = Array(1, 'Rmove users from a list which includes all users enabled twilio softphone', 'custom/include/Twilio/TwilioHooks.php','TwilioHooks','disableSoftPhone');
-
-
-?>
